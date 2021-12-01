1cb3b6231bb53073775bb9e2811aedf73904afc8
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2021-12-02 00:28:08 +0100

    up to icons

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import styles from './OrderOption.module.scss';
+import Icon from '../../common/Icon/Icon';
+import { formatPrice } from '../../../utils/formatPrice';
 
-const OrderOptionIcons = () => (
-    <div>Icons</div>
+const OrderOptionIcons = ({values, currentValue, setOptionValue}) => (
+    <div className={styles.component}>
+      {values.map(value => (
+        <div 
+          className={value.id === currentValue ? styles.iconActive : styles.icon}
+          key={value.id}
+          onClick={event => setOptionValue(value.id)}>
+            <Icon name={value.icon} />
+            {value.name}
+            ({formatPrice(value.price)})
+        </div>
+      ))}
+    </div>
   );
 
 export default OrderOptionIcons;
