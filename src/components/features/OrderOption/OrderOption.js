1ddf1c74903b552d7a31c173e0c73a11fc5d247a
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2021-12-04 00:04:54 +0100

    Start adding text

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -5,12 +5,14 @@ import OrderOptionDropdown from './OrderOptionDropdown';
 import OrderOptionIcons from './OrderOptionIcons';
 import OrderOptionNumber from './OrderOptionNumber';
 import PropTypes from 'prop-types';
+import OrderOptionText from './OrderOptionText';
 
 const optionTypes = {
   dropdown: OrderOptionDropdown,
   icons: OrderOptionIcons,
   checkboxes: OrderOptionCheckboxes,
   number: OrderOptionNumber,
+  text: OrderOptionText,
 };
 
 const OrderOption = ({id, setOrderOption, name, type, ...otherProps}) => {
---
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './OrderOption.module.scss';
+
+const OrderOptionText = () => (
+  
+    <input 
+      type='text' 
+    />
+  
+);
+
+
+export default OrderOptionText;
