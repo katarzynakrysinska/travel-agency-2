32959af4f2eeb7bc471c90182e54f3e354534518
Katarzyna Krysińska <katarzyna.krysinska@onet.pl>
2022-01-04 03:33:01 +0100

    OrderOption -test-part.2

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -3,11 +3,6 @@ import { shallow } from 'enzyme';
 import OrderOption from './OrderOption';
 
 
-
-
-
-
-
 describe('Component OrderOption', () => {
   const expectedType='dropdown';
   const expectedName='name';
@@ -29,3 +24,77 @@ describe('Component OrderOption', () => {
     expect(renderedName).toEqual(expectedName);
   });  
 });
+
+const optionTypes = {
+  dropdown: 'OrderOptionDropdown',
+  icons: 'OrderOptionIcons',
+  checkboxes: 'OrderOptionCheckboxes',
+  number: 'OrderOptionNumber',
+  text: 'OrderOptionText',
+  date: 'OrderOptionDate',
+};
+
+const mockProps = {
+  id: 'abc',
+  name: 'Lorem',
+  values: [
+    {id: 'aaa', icon: 'h-square', name: 'Lorem A', price: 0},
+    {id: 'xyz', icon: 'h-square', name: 'Lorem X', price: 100},
+  ],
+  required: false,
+  currentValue: 'aaa',
+  price: '50%',
+  limits: {
+    min: 0,
+    max: 6,
+  },
+};
+
+const mockPropsForType = {
+  dropdown: {},
+  icons: {},
+  checkboxes: {currentValue: [mockProps.currentValue]},
+  number: {currentValue: 1},
+  text: {},
+  date: {},
+};
+
+//const testValue = mockProps.values[1].id;
+//const testValueNumber = 3;
+
+for(let type in optionTypes){
+  describe(`Component OrderOption with type=${type}`, () => {
+    /* test setup */
+
+    let component;
+    //let subcomponent;
+    //let renderedSubcomponent;
+
+    beforeEach(() => {
+      component = shallow(
+        <OrderOption
+          type={type}
+          {...mockProps}
+          {...mockPropsForType[type]}
+        />,
+      );
+      //subcomponent = component.find(optionTypes[type]);
+      //renderedSubcomponent = subcomponent.dive();
+    });
+
+    /* common tests */
+    it('passes dummy test', () => {
+      console.log(component.debug());
+      expect(1).toBe(1);
+      
+    });
+
+    /* type-specific tests */
+    switch (type) {
+      case 'dropdown': {
+        /* tests for dropdown */
+        break;
+      }
+    }
+  });
+}
