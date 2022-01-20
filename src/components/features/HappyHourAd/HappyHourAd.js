4fbfe6e238ac9d7cd640545a5933069d223912f5
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2022-01-20 02:00:10 +0100

    test2,3

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class HappyHourAd extends React.Component {
   render() { 
+    let {title} = this.props;
     return (
-      <p></p>
+      <div>
+        <h3 className='title'>{title}</h3>
+        <div className='promoDescription'></div>
+      </div>
     );
   }
 }
+
+HappyHourAd.propTypes = {
+  title: PropTypes.string,
+};
 
 export default HappyHourAd;
--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import HappyHourAd from './HappyHourAd';
 
+const select = {
+  title: '.title',
+  promoDescription: '.promoDescription',
+};
 
+const mockProps = {
+  title: 'titleMockProps',
+  promoDescription: 'promoDescriptionMockProps',
+};
 
 describe('Component HappyHourAd', () => {
 
@@ -10,4 +18,20 @@ describe('Component HappyHourAd', () => {
     const component = shallow(<HappyHourAd />);
     expect(component).toBeTruthy();      
   });
+
+  it('should render heading and description', () => {
+    const component = shallow(<HappyHourAd />);
+    expect(component.exists(select.title)).toEqual(true);
+    expect(component.exists(select.promoDescription)).toEqual(true);
+  });
+
+  it('should render title', () => {
+    const component = shallow(<HappyHourAd {...mockProps} />);
+    expect(component.find(select.title).text()).toBe(mockProps.title);
+  });
+
+});
+
+describe('Component HappyHourAd with mocked Date', () => {
+  
 });
