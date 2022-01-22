797bbd1c651a7fc5954358457a047fabffb51981
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2022-01-22 22:06:42 +0100

    Test 4- not working

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
 class HappyHourAd extends React.Component {
+
+  getCountdownTime(){
+    const currentTime = new Date();
+    const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(),
+      currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
+  
+    if(currentTime.getUTCHours() >= 12){
+      nextNoon.setUTCDate(currentTime.getUTCDate() +1);
+    }
+  
+    return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
+  }
   render() { 
     let {title} = this.props;
     return (
       <div>
         <h3 className='title'>{title}</h3>
         <div className='promoDescription'></div>
+        <div className='promoDescription'>{this.getCountdownTime()}</div>
       </div>
     );
   }
@@ -15,6 +29,7 @@ class HappyHourAd extends React.Component {
 
 HappyHourAd.propTypes = {
   title: PropTypes.string,
+  description: PropTypes.string,
 };
 
 export default HappyHourAd;
